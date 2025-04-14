a833bc1207266c6c555d6612079ff394a4630338
thaissabaini <thaissc3@al.insper.edu.br>
2025-04-14 15:03:49 -0300

    resolucao

--- exe3/main.c
+++ exe3/main.c
@@ -20,7 +20,7 @@ void i2c_task(void *p) {
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
-    uint8_t buffer[0];
+
 
     // TODO
     // read id chip BMP280
